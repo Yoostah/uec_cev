1e25a933d4c883b68541dce752091f348cc29228
Thulio <thulio.cormed@gmail.com>
2017-06-14 13:34:32 -0300

    Atualizado javascipt e adicionado botao

--- _script/script.js
+++ _script/script.js
@@ -3,18 +3,26 @@ var l2Escolhido = false;
         
 function mostrarFotoL1 (foto, tipo) {
     if (tipo == 1 && l1Escolhido == false)
-        document.getElementById("L1").src = foto;
+        document.getElementById("L1").src = "_imagens/L1/" + foto + ".png";
     else if (tipo == 2 && l1Escolhido == false){
-        document.getElementById("L1").src = foto;
+        document.getElementById("L1").src = "_imagens/L1/" + foto + ".png";
         l1Escolhido = true;
     }                
 }
 
 function mostrarFotoL2 (foto, tipo) {
     if (tipo == 1 && l2Escolhido == false)
-        document.getElementById("L2").src = foto;
+        document.getElementById("L2").src = "_imagens/L2/" + foto + ".png";
     else if (tipo == 2 && l2Escolhido == false){
-        document.getElementById("L2").src = foto;
+        document.getElementById("L2").src = "_imagens/L2/" + foto + ".png";
         l2Escolhido = true;
+    }
+}
+
+function escolherOutro (lutador) {
+    if (lutador == 'L1'){
+        l1Escolhido = false;
+    }else{
+        l2Escolhido = false;
     }
 }
